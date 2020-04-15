e785121ec970b3ee4f661deea12f11a9eb158bff
Ara Juljulian <ajuljulian@yahoo.com>
2020-04-15 10:52:30 -0700

    work on interactivity. wip.

--- scripts/tab3.py
+++ scripts/tab3.py
@@ -17,76 +17,52 @@ from bokeh.models.widgets import Select, TableColumn, DataTable, CheckboxGroup
 
 def tab3(df, state):
 
-    def get_data_for_state(state):
-        df_one_state = df.where(df['state'] == state)
-        df_one_state = df_one_state.dropna()
-        df_one_state['pct_change_cases'] = df_one_state['cases'].pct_change() * 100
-        df_one_state['pct_change_deaths'] = df_one_state['deaths'].pct_change() * 100
-
-        return df_one_state
-
     def change_state(attr, old, new):
-        pass
-    
+        new_source = state_data[new]
+        source.data = new_source
+        
     tools = HoverTool(
         tooltips=[
+            ('state', '@state'),
             ('date', '@date{%F}'),
             ('cases', '@cases'),
-            ('cases % change', '@pct_change_cases{0.00}%'),
             ('deaths', '@deaths'),
-            ('deaths % change', '@pct_change_deaths{0.00}%')
         ],
         formatters={'@date': 'datetime'},
     )
 
-    states = sorted(list(df['state'].unique()))
-
-    source = ColumnDataSource(data=dict(
-        state=states,
-        data=[get_data_for_state(state) for state in states]
-    ))
-
-    df_one_state = get_data_for_state(state)
-
-    p1 = figure(x_axis_type="datetime", title="Covid-19 Total Cases in {}".format(state),
+    p = figure(x_axis_type="datetime", plot_width=900, plot_height=350, title="Covid-19 Growth",
                toolbar_location=None)
-    
-    select = Select(title="State:", value=states[0], options=states, width=300)
 
     # https://stackoverflow.com/questions/50285405/bokeh-the-widths-of-vertical-bars-doesnt-change
     # for datetime axes, the unit is milliseconds since epoch.  So something like width=0.9 will
     # only display a vertical line.  We either have to make it much larger or use timedelta the
     # way I've done below.
-    p1.vbar(x='date', top='cases', width=timedelta(days=22/24), source=df_one_state.iloc[-40:])
-    p1.add_tools(tools)
+    #p.vbar(x='date', top='cases', width=timedelta(days=22/24), source=df_one_state.iloc[-40:])
 
-    p2 = figure(x_axis_type="datetime", x_range=p1.x_range, title="Covid-19 Cases % Growth in {}".format(state),
-               toolbar_location=None)
-    p2.line(x='date', y='pct_change_cases', source=df_one_state.iloc[-40:])
-    p2.add_tools(tools)
+    p.add_tools(tools)
 
-    p3 = figure(x_axis_type="datetime", x_range=p1.x_range, title="Covid-19 Deaths % Growth in {}".format(state),
-               toolbar_location=None)
-    p3.line(x='date', y='pct_change_deaths', source=df_one_state.iloc[-40:])
-    p3.add_tools(tools)
+    p.xaxis.major_label_orientation = math.pi/2
+    p.xaxis.axis_label = "Date"
+    p.yaxis.axis_label = "Total"
+    p.yaxis.formatter = BasicTickFormatter(use_scientific=False)
+    
+    states = sorted(list(df['state'].unique()))
+    
+    state_data = {}
+    for state in states:
+        df_state = df.where(df['state'] == state).dropna()
+        state_data[state] = df_state[-40:]
+        
+    source = ColumnDataSource(data=state_data[states[0]])
+    
+    select = Select(title="State:", value=states[0], options=states, width=300)
+    select.on_change('value', change_state)
+    
+    p.vbar(x='date', top='cases', width=timedelta(days=22/24), source=source)
 
-    p1.xaxis.formatter.days = '%m/%d/%Y'
-    p1.xaxis.major_label_orientation = math.pi/3
-    p1.xaxis.axis_label = "Date"
-    p1.yaxis.axis_label = "Total"
-
-    p2.xaxis.formatter.days = '%m/%d/%Y'
-    p2.xaxis.major_label_orientation = math.pi/3
-    p2.xaxis.axis_label = "Date"
-    p2.yaxis.axis_label = "Total"
-
-    p3.xaxis.formatter.days = '%m/%d/%Y'
-    p3.xaxis.major_label_orientation = math.pi/3
-    p3.xaxis.axis_label = "Date"
-    p3.yaxis.axis_label = "Total"
-
-    grid = gridplot([[select], [p1],[p2],[p3]], plot_width=900, plot_height=350)
+    grid = gridplot([[select], [p],], plot_width=900, plot_height=350)
  
-    tab = Panel(child=grid, title='Tab 3')
+    tab = Panel(child=grid, title='US States')
     
     return tab
